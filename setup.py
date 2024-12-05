81ebdec6493ac41f5658f5afb5da3d537c1ba2a6
Vishnuu011 <vishnurrajeev@gmail.com>
2024-12-05 20:12:49 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="planthealth_prediction",
+    version="0.0.0",
+    author="vishnu",
+    author_email="vishnurrajeev@gmail.com",
+    packages=find_packages()
+
+)
